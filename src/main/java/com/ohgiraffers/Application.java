5ddd8ec5a8a2800d0179e7525aef65d3465edba7
manho <krossjsnet88@gmail.com>
2024-02-21 21:46:53 +0900

    develop commit

--- src/main/java/com/ohgiraffers/Application.java
+++ src/main/java/com/ohgiraffers/Application.java
@@ -6,6 +6,12 @@ public class Application {
 
         System.out.println("master 생성함.");
 
+        System.out.println("develop branch 생성함.");
+
+        // 이민국 "feature A 생성함"
+        // 김지호 "feature B 생성함"
+        // 김진용 "feature C 생성함"
+
     }
 
 }
