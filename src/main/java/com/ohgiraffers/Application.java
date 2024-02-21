e4caedca5f6d682673261d10e6a6a9a05140b8be
LEEMINKUK <cjcjcj963@gmail.com>
2024-02-21 21:49:09 +0900

    이민국

--- src/main/java/com/ohgiraffers/Application.java
+++ src/main/java/com/ohgiraffers/Application.java
@@ -8,6 +8,8 @@ public class Application {
 
         System.out.println("develop branch 생성함.");
 
+        System.out.println("feature A 생성함. 근데 잘생김");
+
         // 이민국 "feature A 생성함"
         // 김지호 "feature B 생성함"
         // 김진용 "feature C 생성함"
